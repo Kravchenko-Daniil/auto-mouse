95c64193d61152b2326425d52871f16d431cf850
Kravchenko-Daniil <90443315+Kravchenko-Daniil@users.noreply.github.com>
2024-03-07 16:16:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,81 @@
-import pyautogui
-import math
-import keyboard
-import sys
+import pyautogui, keyboard, sys, threading, time, random, os
+from datetime import datetime, timedelta
 
-# Radius
-R = 400
-# measuring screen size
-(x,y) = pyautogui.size()
-# locating center of the screen
-(X,Y) = pyautogui.position(x/2,y/2)
-# offsetting by radius
-pyautogui.moveTo(X+R,Y)
 
-while True:
-    for i in range(360):
+def start_by_path(app):
+    if os.getlogin() == 'krawc':
+        tem = r"D:\Apps\MSTeamsSetup_c_l_.exe"
+        out = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk"
+    else:
+        tem = fr"C:\Users\{os.getlogin()}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams classic.lnk"
+        out = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk"
+
+    if app == "Teams":
+        os.startfile(tem)
+    elif app == "Outlook":
+        os.startfile(out)
+
+
+def mouse_move(x, y):
+    speeds = [0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
+    pyautogui.moveTo(x, y, random.choice(speeds))
+    pyautogui.click()
+
+
+def outlook(end):
+    x = 325
+    y = 350
+    start = datetime.now()
+
+    start_by_path("Outlook")
+    time.sleep(5)
+
+    mouse_move(x, y)
+    time.sleep(1)
+
+    while True:
+        process_time = datetime.now() - start
+        if process_time >= end:
+            break
+
+        mouse_move(x, random.randint(300, 900))
+        time.sleep(1)
+        pyautogui.press('space')
+
+
+def teams(end):
+    x = 450
+    y = 50
+    start = datetime.now()
+
+    start_by_path("Teams")
+    time.sleep(5)
+
+    mouse_move(x, y)
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', '2')
+
+    while True:
+        process_time = datetime.now() - start
+        if process_time >= end:
+            break
+
+        mouse_move(x, random.randint(400, 800))
+        time.sleep(1)
+
+
+def exit_listening():
+    while True:
         if keyboard.is_pressed('F7'):
             sys.exit()
-        # setting pace with a modulus
-        if i%10==0:
-           pyautogui.moveTo(X+R*math.cos(math.radians(i)),Y+R*math.sin(math.radians(i)))
-           # print('a')
 
 
+def apps_launch():
+    while True:
+        teams(timedelta(seconds=300))
+        outlook(timedelta(seconds=300))
+
+
+if __name__ == "__main__":
+    threading.Thread(target=exit_listening, name='killer', daemon=False).start()
+    threading.Thread(target=apps_launch, name='apps', daemon=True).start()
